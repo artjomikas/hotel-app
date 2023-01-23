f46cbda3d6854102db55d510cd301c35d182f144
Artjom Strelkov <strelkov.artjom@mail.ru>
2023-01-23 18:11:36 +0200

    added header with hero slider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import LogoWhite from "../assets/img/logo-white.svg";
+import LogoDark from "../assets/img/logo-dark.svg";
+import { useState, useEffect } from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [header, setHeader] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    });
+  });
+
+  return (
+    <header
+      className={`${
+        header ? "bg-white py-6 shadow-lg" : "bg-transparent py-8"
+      } fixed z-50 w-full transition-all duration-500
+      `}
+    >
+      <div className="container mx-auto flex flex-col items-center gap-y-6 lg:flex-row lg:justify-between lg:gap-y-0">
+        <a href="/">
+          {header ? (
+            <img className="w-[160px]" src={LogoDark} />
+          ) : (
+            <img className="w-[160px]" src={LogoWhite} />
+          )}
+        </a>
+
+        <nav
+          className={`${
+            header ? "text-primary" : "text-white"
+          } flex gap-x-4 font-tertiary tracking-[3px] text-base items-center uppercase lg:gap-x-8
+      `}
+        >
+          {["home", "rooms", "restaurant", "spa", "contact"].map((elem) => {
+            return (
+              <a href="" className="hover:text-accent transition">
+                {elem}
+              </a>
+            );
+          })}
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
